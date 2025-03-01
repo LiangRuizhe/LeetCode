2bf7e4e00be69628e7d1f4b62934c9edd04069da
LiangRuizhe <18741131878@163.com>
2025-03-01 15:02:37 +0800

    Update Leetcode-205-同构字符串.java

--- 哈希/Leetcode-205-同构字符串.java
+++ 哈希/Leetcode-205-同构字符串.java
@@ -1,1 +1,18 @@
-
+//建立一个从字符串s到字符串t的双向映射，如果对应位置上的字符schar和tchar不存在于这个双向映射中则返回false
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> s2t = new HashMap<>();
+        Map<Character,Character> t2s = new HashMap<>();
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            char schar = s.charAt(i);
+            char tchar = t.charAt(i);
+            if((s2t.containsKey(schar) && s2t.get(schar)!=tchar)||(t2s.containsKey(tchar)&&t2s.get(tchar)!=schar)){
+                return false;
+            }
+            s2t.put(schar,tchar);
+            t2s.put(tchar,schar);
+        }
+        return true;
+        }
+}
