868a75d57e9f936bce43b4f77a66095b0544c5a6
LiangRuizhe <18741131878@163.com>
2025-03-01 15:41:41 +0800

    Update Leetcode-0283-移动零.java

--- 双指针/Leetcode-0283-移动零.java
+++ 双指针/Leetcode-0283-移动零.java
@@ -1,14 +1,16 @@
+//双指针，右指针每次向右移动，如果右指针不为零，则赋值到左指针的位置。第一个循环结束后，左指针左侧全为为零数字，左指针右侧到右指针全部赋值为0
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int cur =0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[cur] = nums[i];
-                cur++;
+        int left =0;
+        int right =0;
+        for(;right<nums.length;right++){
+            if(nums[right]!=0){
+                nums[left] = nums[right];
+                left++;
             }
         }
-        for(int i = cur;i<nums.length;i++){
+        for(int i = left;i<right;i++){
             nums[i]=0;
         }
     }
