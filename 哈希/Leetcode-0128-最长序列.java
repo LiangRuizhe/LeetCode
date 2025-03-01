0006c27ac83b32fd0c7c2d76d0a539314fb644d6
LiangRuizhe <18741131878@163.com>
2025-03-01 15:24:48 +0800

    Update Leetcode-0128-最长序列.java

--- 哈希/Leetcode-0128-最长序列.java
+++ 哈希/Leetcode-0128-最长序列.java
@@ -1,5 +1,5 @@
 //对于一个数nums[i]，只有set中不存在nums[i-1]的值，这个数nums[i]才有可能成为最长序列的开头
-//然后循环查找是否包含nums[i]+1
+//然后循环查找是否包含nums[i]+1，记录最长序列的长度
 class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> num_set = new HashSet<Integer>();
