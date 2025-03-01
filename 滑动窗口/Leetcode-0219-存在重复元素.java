0d2c4d987005ffd1f6722f03f1be76986993c150
LiangRuizhe <18741131878@163.com>
2025-03-01 21:57:56 +0800

    Update Leetcode-0219-存在重复元素.java

--- 滑动窗口/Leetcode-0219-存在重复元素.java
+++ 滑动窗口/Leetcode-0219-存在重复元素.java
@@ -1,1 +1,17 @@
-
+//使用滑动窗口维护一个长度为k大小的set集合,检查是否存在重复
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        int len = nums.length;
+        for(int i=0; i<len; i++){
+            if(i>k){
+                set.remove(nums[i-1-k]);
+            }
+            if(set.add(nums[i])==false){
+              //向set中使用add方法加入重复元素会返回false
+                   return true;
+            }
+        }
+        return false;   
+    }
+}
