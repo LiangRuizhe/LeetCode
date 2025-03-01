486c0b0ba81fa1a4bcc602e0d5b0e7ecb0b26b55
LiangRuizhe <18741131878@163.com>
2025-03-01 14:51:58 +0800

    Update Leetcode-049-字母异位词分组.java

--- 哈希/Leetcode-049-字母异位词分组.java
+++ 哈希/Leetcode-049-字母异位词分组.java
@@ -1,4 +1,5 @@
 //互为字母异位词的两个字符串包含的字母相同,将字符串数组排序后放入HashMap中
+//可以将排序之后的字符串作为哈希表的键，包含字母异位词的字符串数组作为值
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String,List<String>> map = new HashMap<String,List<String>>();
