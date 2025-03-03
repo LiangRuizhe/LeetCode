3f1df995df6989859096a9ddafd19fedb08cc4a8
LiangRuizhe <18741131878@163.com>
2025-03-03 17:37:36 +0800

    Update Leetcode-0064-最小路径和.java

--- 多维动态规划/Leetcode-0064-最小路径和.java
+++ 多维动态规划/Leetcode-0064-最小路径和.java
@@ -1,3 +1,5 @@
+//状态转移方程：dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + grid[i][j]
+//边界条件： dp[0][0]=grid[0][0]; dp[i][0] = dp[i-1][0] + grid[i][0]; dp[0][j] = dp[0][j-1] + grid[0][j];
 class Solution {
     public int minPathSum(int[][] grid) {
         if(grid == null || grid.length == 0 || grid[0].length ==0){
