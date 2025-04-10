afd78d1cd4fd128b9292115341f6c7b453f364f6
LiangRuizhe <18741131878@163.com>
2025-04-10 23:42:47 +0800

    Update Leetcode-0141-环形链表.java

--- 链表/Leetcode-0141-环形链表.java
+++ 链表/Leetcode-0141-环形链表.java
@@ -1,8 +1,9 @@
+//快慢指针终将相遇
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null)
         return false;
-        ListNode fast = head.next;
+        ListNode fast = head.next;//避免while循环不运行
         ListNode slow = head;
         while(fast!=slow){
             if(fast==null || fast.next ==null)
