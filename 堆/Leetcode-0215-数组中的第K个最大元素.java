04c71faf6b052b89a986b99517149426d606b033
LiangRuizhe <18741131878@163.com>
2025-04-13 00:19:25 +0800

    Update Leetcode-0215-数组中的第K个最大元素.java

--- 堆/Leetcode-0215-数组中的第K个最大元素.java
+++ 堆/Leetcode-0215-数组中的第K个最大元素.java
@@ -16,3 +16,35 @@ class Solution1 {
         return minHeap.peek();
     }
 }
+//快速排序，三路划分
+class Solution2 {
+    public int findKthLargest(int[] nums, int k) {
+      List<Integer> ans = new ArrayList<>();
+      for(int i=0;i<nums.length;i++){
+        ans.add(nums[i]);
+      }
+      return QuickSelect(ans, k);
+        
+    }
+    public int QuickSelect(List<Integer> nums,int k){
+        Random ran = new Random();
+        int pivot = nums.get(ran.nextInt(nums.size()));
+        List<Integer> bigger = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> smaller = new ArrayList<>();
+        for(int num:nums){
+            if(num>pivot)
+            bigger.add(num);
+            else if(num<pivot)
+            smaller.add(num);
+            else
+             equal.add(num);
+        }
+        if(k<=bigger.size())
+        return QuickSelect(bigger, k);//注意判断bigger的边界条件
+        if(nums.size()-smaller.size()<k){
+        return QuickSelect(smaller, k - nums.size() + smaller.size());
+        }
+        return pivot;
+    }
+}
